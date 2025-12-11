281efdaf57993b8715b875fa11d953ad9ad753dc
Marcos <marcos.a.m@live.com>
2025-12-10 23:53:45 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Jornda de dados")
+
+#variáveis
+idade = 34
+nome = "Marcos"
+
+print(idade)
+
+nova_idade = idade + 50
+print(nova_idade)
+
+#Lista
+lista = [1,2,3,4,5]
+
+#Método
+lista_invertida = lista.reverse()
